da5d76d145caabfef15bd78b018a0c63fce7f0ba
wangmin <wangmin@trs.com.cn>
2017-07-03 18:51:40 +0800

    修改log

--- Demo/Demo/AppDelegate.m
+++ Demo/Demo/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    NSLog(@"123456789");
+    NSLog(@"123456789111111");
     return YES;
 }
 
