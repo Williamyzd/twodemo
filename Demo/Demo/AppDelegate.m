bcfda74e63ef1535587420672c9387f1575286af
wangmin <wangmin@trs.com.cn>
2017-07-04 13:07:25 +0800

    xiu gai log

--- Demo/Demo/AppDelegate.m
+++ Demo/Demo/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    NSLog(@"123456789111111errrrraisiasciaakjrrrrr");
+    NSLog(@"oooooo");
     return YES;
 }
 
